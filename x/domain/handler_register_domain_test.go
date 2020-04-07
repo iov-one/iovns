c7d4f7ff7bc6080153508334f86bf75dd16c9d5c
frojdidymylja <frojdi.dymylja@gmail.com>
2020-04-07 21:23:03 +0200

    implement second test draft

--- x/domain/handler_register_domain_test.go
+++ x/domain/handler_register_domain_test.go
@@ -13,6 +13,42 @@ import (
 	"testing"
 )
 
+// subTest defines a test runner
+type subTest struct {
+	// Name is the name of the test
+	Name string
+	// BeforeTest is the function run before doing the test,
+	// used for example to store state, like configurations etc.
+	// Ignored if nil
+	BeforeTest func(t *testing.T, k Keeper, ctx sdk.Context)
+	// Test runs the actual test
+	Test func(t *testing.T, k Keeper, ctx sdk.Context)
+	// AfterTest performs actions after the test is run, it can
+	// be used to check if the state after Test is run matches
+	// the result we expect.
+	// Ignored if nil
+	AfterTest func(t *testing.T, k Keeper, ctx sdk.Context)
+}
+
+func runTests(t *testing.T, tests []subTest) {
+	keeper, ctx := newTestKeeper(t, true)
+	for _, test := range tests {
+		// run sub subTest
+		t.Run(test.Name, func(t *testing.T) {
+			// run before subTest
+			if test.BeforeTest != nil {
+				test.BeforeTest(t, keeper, ctx)
+			}
+			// run actual subTest
+			test.Test(t, keeper, ctx)
+			// run after subTest
+			if test.AfterTest != nil {
+				test.AfterTest(t, keeper, ctx)
+			}
+		})
+	}
+}
+
 // newTestCodec generates a codec for keeper module
 func newTestCodec() *codec.Codec {
 	// we should register this codec for all the modules
@@ -38,7 +74,7 @@ func newTestKeeper(t *testing.T, isCheckTx bool) (Keeper, sdk.Context) {
 	ms.MountStoreWithDB(configurationStoreKey, sdk.StoreTypeIAVL, mdb) // mount configuration module
 	ms.MountStoreWithDB(accountStoreKey, sdk.StoreTypeIAVL, mdb)       // mount account module
 	ms.MountStoreWithDB(domainStoreKey, sdk.StoreTypeIAVL, mdb)        // mount domain module
-	// test if empty
+	// test no errors
 	require.Nil(t, ms.LoadLatestVersion())
 	// create config keeper
 	confKeeper := configuration.NewKeeper(cdc, configurationStoreKey, nil)
@@ -50,34 +86,44 @@ func newTestKeeper(t *testing.T, isCheckTx bool) (Keeper, sdk.Context) {
 	return NewKeeper(cdc, domainStoreKey, accountKeeper, confKeeper, nil), ctx
 }
 
-func TestHandleMsgRegisterDomain(t *testing.T) {
-	type configurationSetter interface {
-		SetConfig(ctx sdk.Context, config configuration.Config)
+func TestMsgRegisterDomain(t *testing.T) {
+	test := subTest{
+		Name: "success",
+		BeforeTest: func(t *testing.T, k Keeper, ctx sdk.Context) {
+			// add config
+			type configurationSetter interface {
+				SetConfig(ctx sdk.Context, config configuration.Config)
+			}
+			// check if the configuration keeper is also a config setter
+			configSetter, ok := k.ConfigurationKeeper.(configurationSetter)
+			if !ok {
+				t.Fatalf("handleMsgRegisterDomain() cannot cast configuration keeper to configuration setter: got uncastable type: %T", k.ConfigurationKeeper)
+			}
+			// set config
+			configSetter.SetConfig(ctx, configuration.Config{
+				Owner:                  nil,
+				ValidDomain:            "^(.*?)?",
+				ValidName:              "",
+				ValidBlockchainID:      "",
+				ValidBlockchainAddress: "",
+				DomainRenew:            0,
+			})
+		},
+		Test: func(t *testing.T, k Keeper, ctx sdk.Context) {
+			_, err := handleMsgRegisterDomain(ctx, k, MsgRegisterDomain{
+				Name:         "domain",
+				Admin:        nil,
+				HasSuperuser: true,
+				Broker:       nil,
+				AccountRenew: 10,
+			})
+			if err != nil {
+				t.Fatalf("handleMsgRegisterDomain() got error: %s", err)
+			}
+		},
+		AfterTest: func(t *testing.T, k Keeper, ctx sdk.Context) {
+
+		},
 	}
-	keeper, ctx := newTestKeeper(t, true)
-	// check if the configuration keeper is also a config setter
-	configSetter, ok := keeper.ConfigurationKeeper.(configurationSetter)
-	if !ok {
-		t.Fatalf("handleMsgRegisterDomain() cannot cast configuration keeper to configuration setter: got uncastable type: %T", keeper.ConfigurationKeeper)
-	}
-	// set config
-	configSetter.SetConfig(ctx, configuration.Config{
-		Owner:                  nil,
-		ValidDomain:            "^(.*?)?",
-		ValidName:              "",
-		ValidBlockchainID:      "",
-		ValidBlockchainAddress: "",
-		DomainRenew:            0,
-	})
-	// do test
-	_, err := handleMsgRegisterDomain(ctx, keeper, MsgRegisterDomain{
-		Name:         "domain",
-		Admin:        nil,
-		HasSuperuser: true,
-		Broker:       nil,
-		AccountRenew: 10,
-	})
-	if err != nil {
-		t.Fatalf("handleMsgRegisterDomain() got error: %s", err)
-	}
+	runTests(t, []subTest{test})
 }
