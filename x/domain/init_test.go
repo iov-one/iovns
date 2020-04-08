ea1010d3f7a92d337e9b7070c8ec67c02e7314f5
frojdidymylja <frojdi.dymylja@gmail.com>
2020-04-08 12:40:35 +0200

    typo fix

--- x/domain/init_test.go
+++ x/domain/init_test.go
@@ -97,14 +97,14 @@ type configurationSetter interface {
 }
 
 // getConfigSetter exposes the configurationSetter interface
-// allowing yhe module to set configuration state, this should only
+// allowing the module to set configuration state, this should only
 // be used for tests and will panic if the keeper provided can not
 // be cast to configurationSetter
 func getConfigSetter(keeper types.ConfigurationKeeper) configurationSetter {
 	// check if the configuration keeper is also a config setter
 	configSetter, ok := keeper.(configurationSetter)
 	if !ok {
-		panic(fmt.Sprintf("handleMsgRegisterDomain() cannot cast configuration keeper to configuration setter: got uncastable type: %T", keeper))
+		panic(fmt.Sprintf("cannot cast configuration keeper to configuration setter: got uncastable type: %T", keeper))
 	}
 	return configSetter
 }
