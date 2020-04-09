b9ae3151ebe09f7b05b9372259934a59b87659aa
frojdidymylja <frojdi.dymylja@gmail.com>
2020-04-09 15:05:42 +0200

    add docs

--- x/domain/genesis.go
+++ x/domain/genesis.go
@@ -6,14 +6,19 @@ import (
 	"github.com/iov-one/iovnsd/x/domain/types"
 )
 
+// GenesisState represents the state of the domain module
 type GenesisState struct {
+	// DomainRecords contains the records of registered domains
 	DomainsRecords []types.Domain `json:"domain_records"`
 }
 
+// NewGenesisState builds a genesis state including the domains provided
 func NewGenesisState(domains []types.Domain) GenesisState {
 	return GenesisState{DomainsRecords: domains}
 }
 
+// ValidateGenesis validates a genesis state
+// checking for domain validity and no domain name repetitions
 func ValidateGenesis(data GenesisState) error {
 	namesSet := make(map[string]struct{}, len(data.DomainsRecords))
 	for _, domain := range data.DomainsRecords {
@@ -28,12 +33,7 @@ func ValidateGenesis(data GenesisState) error {
 	return nil
 }
 
-// validateDomain checks if a domain is valid or not
-func validateDomain(d types.Domain) error {
-	// TODO fill
-	return nil
-}
-
+// DefaultGenesisState creates an empty genesis state for the domain module
 func DefaultGenesisState() GenesisState {
 	// TODO remove.
 	return GenesisState{DomainsRecords: []types.Domain{
@@ -48,12 +48,14 @@ func DefaultGenesisState() GenesisState {
 	}}
 }
 
+// InitGenesis builds a state from GenesisState
 func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) {
 	for _, domain := range data.DomainsRecords {
 		keeper.SetDomain(ctx, domain)
 	}
 }
 
+// ExportGenesis saves the state of the domain module
 func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
 	var records []types.Domain
 	iterator := k.IterateAll(ctx)
@@ -62,4 +64,10 @@ func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
 		records = append(records, domain)
 	}
 	return GenesisState{DomainsRecords: records}
+}
+
+// validateDomain checks if a domain is valid or not
+func validateDomain(d types.Domain) error {
+	// TODO fill
+	return nil
 }
