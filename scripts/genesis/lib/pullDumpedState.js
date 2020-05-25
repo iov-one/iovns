0822b608fd87a9bf5b0ba865eb2de60b1eec0b20
Dave Puchyr <dave@slim.avaritia.com>
2020-05-25 20:10:54 +0200

    Add gitPullGist()

--- scripts/genesis/lib/pullDumpedState.js
+++ scripts/genesis/lib/pullDumpedState.js
@@ -1,30 +1,11 @@
-import { spawn } from "child_process";
-import fs from "fs";
-import path from "path";
+import gitPullGist from "./gitPullGist"
 
 "use strict";
 
 
 const pullDumpedState = async () => {
-   const pwd = path.dirname( process.argv[1].indexOf( "build" ) !== -1 ? process.argv[1].replace( "build", "" ) : process.argv[1] );
-
-   process.chdir( path.join( pwd, "data", "dump" ) );
-
-   const git = spawn( "git", [ "pull" ] );
-   const stdout = [];
-
-   git.once( "exit", code => {
-      if ( code ) console.error( stdout.join( "\n" ) );
-   } );
-   git.once( "error", error => {
-      throw new Error( error );
-   } );
-
-   for await ( const data of git.stdout ) {
-      stdout.push( String( data ) );
-   }
-
-   const state = JSON.parse( fs.readFileSync( path.join( pwd, "data", "dump", "dump.json" ), "utf-8" ) );
+   const gist = await gitPullGist( [ "data", "dump", "dump.json" ] );
+   const state = JSON.parse( gist );
 
    return state;
 }
--- scripts/genesis/lib/pullPremiums.js
+++ scripts/genesis/lib/pullPremiums.js
@@ -1,33 +1,15 @@
-import { spawn } from "child_process";
-import fs from "fs";
-import path from "path";
+import gitPullGist from "./gitPullGist"
 
 "use strict";
 
 
 const pullPremiums = async () => {
-   const pwd = path.dirname( process.argv[1].indexOf( "build" ) !== -1 ? process.argv[1].replace( "build", "" ) : process.argv[1] );
-
-   process.chdir( path.join( pwd, "data", "premium" ) );
-
-   const git = spawn( "git", [ "pull" ] );
-   const stdout = [];
-
-   git.once( "exit", code => {
-      if ( code ) console.error( stdout.join( "\n" ) );
-   } );
-   git.once( "error", error => {
-      throw new Error( error );
-   } );
-
-   for await ( const data of git.stdout ) {
-      stdout.push( String( data ) );
-   }
-
-   const text = fs.readFileSync( path.join( pwd, "data", "premium", "premium.csv" ), "utf-8" );
-   const premiums = text.split( "\n" ).reduce( ( accumulator, line ) => {
+   const gist = await gitPullGist( [ "data", "premium", "premium.csv" ] ).catch( e => { throw e } );
+   const premiums = gist.split( "\n" ).reduce( ( accumulator, line ) => {
       const columns = line.split( /,/g );
+
       accumulator[columns.shift()] = columns;
+
       return accumulator;
    }, {} );
 
