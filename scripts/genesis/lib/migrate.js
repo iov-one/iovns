2b65e51b019aa524e503c515acd091a63a5e8c5c
Dave Puchyr <dave@slim.avaritia.com>
2020-05-26 13:21:18 +0200

    Add labelMultisigs()

--- scripts/genesis/lib/migrate.js
+++ scripts/genesis/lib/migrate.js
@@ -14,13 +14,30 @@ export const burnTokens = dumped => {
 };
 
 /**
+ * Adds an "//id" property to multisig accounts
+ * @param {Object} dumped - the state of the weave-based chain
+ * @param {Object} multisigs - the state of the weave-based chain
+ */
+export const labelMultisigs = ( dumped, multisigs ) => {
+   Object.keys( multisigs ).forEach( iov1 => {
+      const index = dumped.cash.findIndex( wallet => wallet.address == iov1 );
+
+      if ( index == -1 ) throw new Error( `Couldn't find ${iov1} in dumped.cash.` );
+
+      dumped.cash[index]["//id"] = multisigs[iov1]["//name"];
+   } );
+}
+
+/**
  * Performs all the necessary transformations to migrate from the weave-based chain to a cosmos-sdk-based chain.
  * @param {Object} args - various objects required for the transformation
  */
 export const migrate = args => {
    const dumped = args.dumped;
    const genesis = args.genesis;
+   const multisigs = args.multisigs;
    const osaka = args.osaka;
 
    burnTokens( dumped );
+   labelMultisigs( dumped, multisigs );
 };
--- scripts/genesis/test/lib/migrate.test.js
+++ scripts/genesis/test/lib/migrate.test.js
@@ -1,4 +1,4 @@
-import { burnTokens, migrate } from "../../lib/migrate";
+import { burnTokens, labelMultisigs, migrate } from "../../lib/migrate";
 
 "use strict";
 
@@ -36,6 +36,18 @@ describe( "Tests ../../lib/migrate.js.", () => {
       genutil: {},
       gov: {},
    };
+   const multisigs = {
+      iov1k0dp2fmdunscuwjjusqtk6mttx5ufk3zpwj90n: {
+         "//name": "reward fund",
+         address: "cond:gov/rule/0000000000000002",
+         star1: "star1rewards",
+      },
+      iov1zd573wa38pxfvn9mxvpkjm6a8vteqvar2dwzs0: {
+         "//name": "IOV SAS employee bonus pool/colloboration appropriation pool",
+         address: "cond:multisig/usage/0000000000000002",
+         star1: "star1bonuses",
+      },
+   };
 
    it( `Should burn tokens.`, async () => {
       const copied = JSON.parse( JSON.stringify( dumped ) );
@@ -48,7 +60,17 @@ describe( "Tests ../../lib/migrate.js.", () => {
       expect( copied.cash.length ).toEqual( dumped.cash.length - 1 );
    } );
 
+   it( `Should label multisig accounts.`, async () => {
+      labelMultisigs( dumped, multisigs );
+
+      Object.keys( multisigs ).forEach( iov1 => {
+         const multisig = dumped.cash.find( wallet => wallet.address == iov1 );
+
+         expect( multisig["//id"] ).toEqual( multisigs[iov1]["//name"] );
+      } );
+   } );
+
    it( `Should migrate.`, async () => {
-      migrate( { dumped, genesis } );
+      migrate( { dumped, genesis, multisigs } );
    } );
 } );
