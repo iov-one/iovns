85c0666fb849df57f519daaecb551a3a7ca12598
Dave Puchyr <dave@slim.avaritia.com>
2020-06-01 13:28:08 +0200

    Add another test to fetchIndicativeSendsTo

--- scripts/genesis/test/lib/fetchIndicativeSendsTo.test.js
+++ scripts/genesis/test/lib/fetchIndicativeSendsTo.test.js
@@ -15,4 +15,14 @@ describe( "Tests ../../lib/fetchIndicativeSendsTo.js.", () => {
 
       compareObjects( indicative, sends );
    } );
+
+   it( `Should match star1's first 2 incoming payments.`, async () => {
+      const indicative = [
+         { "hash": "e0d65bc5377e0806de18f76e07c3234632fad570a799c1063df1f69809bf4337", "block_height": 65609, "message": { "path": "cash/send", "details": { "memo": "star1cnywewxct2p4d5j2fapgkse6yxgh7ecnj4uwpu", "amount": { "whole": 1, "ticker": "IOV" }, "source": "iov1yhk8qqp3wsdg7tefd8u457n9zqsny4nqzp6960", "destination": "iov10v69k57z2v0pr3yvtr60pp8g2jx8tdd7f55sv6" } } },
+         { "hash": "20894f0429901e402bb0520d117da9b64dacce2a97b647c66645bf6436af17d7", "block_height": 67029, "message": { "path": "cash/send", "details": { "memo": "star19m9ufykj5ur67l822fpxvz49p535wp3j0m5v3h", "amount": { "ticker": "IOV", "fractional": 1 }, "source": "iov1a9duw7yyxdfh8mrjxmuc0slu8a48muvxkcxvg8", "destination": "iov10v69k57z2v0pr3yvtr60pp8g2jx8tdd7f55sv6" } } }
+      ];
+      const sends = await fetchIndicativeSendsTo( "iov10v69k57z2v0pr3yvtr60pp8g2jx8tdd7f55sv6", /star1[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{38}/ );
+
+      compareObjects( indicative, sends );
+   } );
 } );
