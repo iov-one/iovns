edbc1ebefa3972caa066f6703991570fc03a37dd
davepuchyr <dave.puchyr@avaritia.com>
2020-06-14 20:06:41 +0200

    Minimally update the README now that the repo is public (#205)
    * Tweak blurb; fix docker instructions
    
    * Minor tweaks
    
    * Fix readme docker command
    
    Co-authored-by: Dave Puchyr <dave@debt.avaritia.com>
    Co-authored-by: orkunkl <kulceorkun@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,10 +2,14 @@
 
 set -o nounset
 
+echo -n "This script will remove all existing iovns configurations, wallets etc. Do you want to proceed? [Y/n] "
+read YN
+if ! echo $YN | grep -v -q -i "n"; then exit; fi
+
 rm -rf "$HOME/.iovnscli"
 rm -rf "$HOME/.iovnsd"
 # init config files
-iovnsd init "$MONIKER" --chain-id iovns
+iovnsd init "$(hostname)" --chain-id iovns
 # configure cli
 iovnscli config chain-id iovns
 iovnscli config output json
