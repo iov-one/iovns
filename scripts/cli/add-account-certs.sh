8b40f43ed2a6e571965deb5132c37043402f6a75
orkunkl <kulceorkun@gmail.com>
2020-06-23 08:43:16 +0300

    extra

--- scripts/cli/add-account-certs.sh
+++ scripts/cli/add-account-certs.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-iovnscli tx domain add-certs --domain ${DOMAIN} --cert-file ./cert.json --from ${WALLET1} --broadcast-mode block --gas-prices 10.0uvoi -y
+iovnscli tx domain add-certs --domain "${DOMAIN}" --cert-file ./cert.json --from "${WALLET1}" --broadcast-mode block --gas-prices 10.0"${DENOM}" -y
--- scripts/cli/register-domain.sh
+++ scripts/cli/register-domain.sh
@@ -1,1 +1,1 @@
-iovnscli tx domain register-domain  --domain ${DOMAIN} --from ${WALLET1} --gas-prices 10.0uvoi --broadcast-mode block -y
+iovnscli tx domain register-domain  --domain "${DOMAIN}" --from "${WALLET1}" --gas-prices 10.0"${DENOM}" --broadcast-mode block -y
