f959a8574c13e351ef385f9f168087fa76fd0dec
frojdidymylja <frojdi.dymylja@gmail.com>
2020-07-18 12:50:16 +0200

    chore: enhance genesis state test

--- x/starname/genesis_test.go
+++ x/starname/genesis_test.go
@@ -1,6 +1,7 @@
 package starname
 
 import (
+	"encoding/json"
 	"github.com/iov-one/iovns/tutils"
 	"github.com/iov-one/iovns/x/starname/keeper"
 	"github.com/iov-one/iovns/x/starname/keeper/executor"
@@ -9,7 +10,7 @@ import (
 )
 
 func TestExportGenesis(t *testing.T) {
-
+	expected := `{"domains":[{"name":"test","admin":"cosmos1ze7y9qwdddejmy7jlw4cymqqlt2wh05ytm076d","valid_until":100,"type":"open","broker":""}],"accounts":[{"domain":"test","name":"","owner":"cosmos1ze7y9qwdddejmy7jlw4cymqqlt2wh05ytm076d","valid_until":100,"resources":null,"certificates":null,"broker":"","metadata_uri":""},{"domain":"test","name":"test","owner":"cosmos1ze7y9qwdddejmy7jlw4cymqqlt2wh05ytm076d","valid_until":100,"resources":null,"certificates":null,"broker":"","metadata_uri":""}]}`
 	k, ctx, _ := keeper.NewTestKeeper(t, true)
 	executor.NewDomain(ctx, k, types.Domain{
 		Name:       "test",
@@ -25,5 +26,11 @@ func TestExportGenesis(t *testing.T) {
 		ValidUntil:  100,
 		MetadataURI: "",
 	}).Create()
-	_ = ExportGenesis(ctx, k)
+	b, err := json.Marshal(ExportGenesis(ctx, k))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b) != expected {
+		t.Fatal("unexpected genesis state")
+	}
 }
