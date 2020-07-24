95d335b6c2c87d39f3c8baf6adbe98cdd3b3849b
Dave Puchyr <dave@slim.avaritia.com>
2020-07-24 18:21:54 +0200

    Pull eligible accounts

--- scripts/genesis/rebates.js
+++ scripts/genesis/rebates.js
@@ -1,10 +1,17 @@
-import { Client } from "pg";
-import YAML from "yaml";
-import fs from "fs";
 import { atob } from "Base64";
+import { Client } from "pg";
+import fetch from "node-fetch";
+import fs from "fs";
+import YAML from "yaml";
 
 
 const main = async () => {
+   // upgrade recompense was announced on May 25, so pull accounts that existed as of that date
+   const dump = await fetch( "https://gist.githubusercontent.com/davepuchyr/bf9ab1d2d9ca70326cf58c7c70376554/raw/2c2c7b5694cfeab9dfab479726cefcafa83520c5/dump.json" );
+   const dumped = await dump.json();
+   const eligible = dumped.cash;
+
+   // connect to the block-metrics db
    const yaml = fs.readFileSync( "/run/user/500/keybase/kbfs/team/iov_one.blockchain/credentials/testnet-common/block-metrics-db-settings.yaml", "utf8" )
    const secrets = YAML.parse( yaml );
    const client = new Client( {
