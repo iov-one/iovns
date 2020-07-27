bc7adb767eeae022a60f997fcc7077d9062930bd
davepuchyr <dave.puchyr@avaritia.com>
2020-07-27 19:45:38 +0200

    Enable all tests (#282)
    * Add iovnsd-config.sh
    
    * Add transfer-owned.sh
    
    `bash -x ./transfer-owned.sh` gives reasonably easy to read output.
    
    * Test transfers only
    
    * Fix TransferFlush CLI test
    
    * Drop transfer-owned.sh
    
    It works, so no need to pollute the scripts directory.
    
    * Enable all test
    
    * change: enhance crud store
    
    * chore: update CHANGELOG.md
    
    * Skip timeout commit for 5x faster testing
    
    * Fix skip_timeout_commit
    
    * Add skip_timeout_commit and rest-server to iovnsd-config.sh
    
    * Set timeout_commit instead of skipping it
    
    * fix: filter returns primary key if specified
    
    * fix: finding smallest set in filters
    
    * chore: update CHANGELOG.md
    
    Co-authored-by: Dave Puchyr <dave@slim.avaritia.com>
    Co-authored-by: Frojdi Dymylja <frojdi.dymylja@gmail.com>

--- scripts/integration/test/REST.test.js
+++ scripts/integration/test/REST.test.js
@@ -324,7 +324,7 @@ describe( "Tests the REST API.", () => {
    } );
 
 
-   it.skip( `Should register a domain, register an account, transfer the domain with reset flag 1 (TransferOwned), and query domainInfo.`, async () => {
+   it( `Should register a domain, register an account, transfer the domain with reset flag 1 (TransferOwned), and query domainInfo.`, async () => {
       const transferFlag = "1";
       const domain = `domain${Math.floor( Math.random() * 1e9 )}`;
       const name = `${Math.floor( Math.random() * 1e9 )}`;
