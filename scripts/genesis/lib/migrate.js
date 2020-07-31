bbfa68b4f27c96747934fcf4156fb495d4552112
Dave Puchyr <dave@slim.avaritia.com>
2020-07-31 14:40:54 +0200

    Add ATEAM account and validator gentx

--- scripts/genesis/lib/migrate.js
+++ scripts/genesis/lib/migrate.js
@@ -405,6 +405,22 @@ export const patchGalaxynet = genesis => {
    // add other test accounts
    const accounts = [
       {
+         "//name": "ATEAM",
+         "type": "cosmos-sdk/Account",
+         "value": {
+            "address": "star1yef3tuv6jdzjp73gcfdr5h64jfpk2mqmyphp4j",
+            "coins": [
+               {
+                  "denom": "uiov",
+                  "amount": "20100000000"
+               }
+            ],
+            "public_key": null,
+            "account_number": "0",
+            "sequence": "0"
+         }
+      },
+      {
          "//name": "Cosmostation",
          "type": "cosmos-sdk/Account",
          "value": {
