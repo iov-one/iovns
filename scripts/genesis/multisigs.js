42a10179a789200bd06fd983e63a115f79d13fe8
Dave Puchyr <dave@slim.avaritia.com>
2020-08-03 11:26:30 +0200

    Read names/keys from multisig_pubkeys.txt

--- scripts/genesis/multisigs.js
+++ scripts/genesis/multisigs.js
@@ -1,4 +1,5 @@
 import { spawnSync } from "child_process";
+import fs from "fs";
 
 
 const iovnscli = args => {
@@ -11,39 +12,49 @@ const iovnscli = args => {
 
 
 const main = async () => {
-   const pubkeys = {
-      a: "starpub1addwnpepqww83fu9wdrswn0m2x9uq9df4ml78mtd333u3w9txq2auc7lpgens7259gh", // antoine
-      b: "starpub1addwnpepqtzmf3cy284sn7s7rmcu7qpkg6j6pcpjpanpr67nym88ctltvm3n7jwh63a", // benjamin
-      d: "starpub1addwnpepqvpmxxwmnyl3ng6sc2qynyv2866spmp6an6zhhe97hjgz873f8w2uncy6ym", // dave
-      k: "starpub1addwnpepq2lhq7m62tmmr5acqrwcdnlzu6mgng8cydfmfm9ckuaxvth6wxz3cnp4kln", // karim
-      o: "starpub1addwnpepqd5vtc5j8sl0mxmvxa7ydu4flwh5taxnea423q7d9qup548snshpzyp9z58", // olivier
-   };
-   const users = {
-      a: "antoine",
-      b: "benjamin",
-      d: "dave",
-      k: "karim",
-      o: "olivier",
-   };
+   // read pubkeys
+   const raw = fs.readFileSync( "multisig_pubkeys.txt", "utf8" );
+   const lines = raw.split( "\n" );
+   const reKey = /pubkey: (.*)/;
+   const reName = /- name: (.*)/;
+   const pubkeys = {};
+
+   for ( let i = 0, n = lines.length; i < n; ++i ) {
+      const line = lines[i];
+
+      if ( line.indexOf( "- name:" ) != -1 ) {
+         const name = line.match( reName )[1].trim();
+         const key = lines[++i].match( reKey )[1].trim();
+
+         pubkeys[name] = key;
+      }
+   }
+
+   // get user
    const user = await new Promise( ( resolve, reject ) => {
       console.log( `Who are you? ("a" for "Antoine", "b" for "Benjamin", etc) [abdko]` );
 
       process.stdin.resume();
       process.stdin.once( "data", abdko => {
          const input = String( abdko ).trim().toLocaleLowerCase();
-         const pubkey = pubkeys[input];
+         const users = {
+            a: "antoine",
+            b: "benjamin",
+            d: "dave*iov",
+            k: "karim",
+            o: "olivier",
+         }
+         const user = users[input.slice( 0, 1 )];
 
-         if ( !pubkey ) reject( new Error( `'${input}' is not one of a, b, d, k, or o.` ) );
-
-         resolve( users[input] );
+         user && resolve( user ) || reject( new Error( `'${input}' is not one of a, b, d, k, or o.` ) );
       } );
    } );
+
+   // create needed keys
    const have = iovnscli( [ "keys", "list" ] ).map( value => value.name );
    const need = Object.keys( pubkeys ).filter( key => !have.includes( key ) );
 
-   need.forEach( key => {
-      const name = users[key];
-
+   need.forEach( name => {
       if ( name == user ) throw new Error( `Key '${user}' should exist already!  Did you do 'iovnscli keys add ${user} --ledger'?` );
    } );
 };
