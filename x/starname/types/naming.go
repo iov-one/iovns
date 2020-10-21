dc2cf3760b13444955fdd238e2fd70570e9ba446
Frojdi Dymylja <33157909+fdymylja@users.noreply.github.com>
2020-10-21 14:03:14 +0200

    add: events on x/starname handlers (#366)
    * add: events for domain registration
    
    * add: events for account and domain handlers
    
    * chore: update CHANGELOG.md
    
    * add: scoped event manager
    
    * Drop unreachable code
    
    Co-authored-by: Dave Puchyr <dave@slim.avaritia.com>

--- x/starname/types/naming.go
+++ x/starname/types/naming.go
@@ -14,3 +14,23 @@ const (
 	// DefaultParamSpace defines the key for the default param space
 	DefaultParamSpace = ModuleName
 )
+
+// Events attribute keys
+const (
+	AttributeKeyDomainName  = "domain_name"
+	AttributeKeyAccountName = "account_name"
+	AttributeKeyDomainType  = "domain_type"
+	AttributeKeyFeePaid     = "paid_fees"
+	AttributeKeyFeePayer    = "fee_payer"
+	AttributeKeyOwner       = "owner"
+
+	AttributeKeyNewCertificate          = "new_certificate"
+	AttributeKeyDeletedCertificate      = "deleted_certificate"
+	AttributeKeyNewResources            = "new_resources"
+	AttributeKeyNewMetadata             = "new_metadata"
+	AttributeKeyTransferAccountNewOwner = "new_account_owner"
+	AttributeKeyTransferAccountReset    = "transfer_account_reset"
+
+	AttributeKeyTransferDomainNewOwner = "new_domain_owner"
+	AttributeKeyTransferDomainFlag     = "transfer_domain_flag"
+)
